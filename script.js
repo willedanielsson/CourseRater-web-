e00d115ae8e6b17b76e584826764ef96a6d102ca
William Danielsson <William@williams-mbp.lan>
2014-12-10 20:29:59 +0100

    Single page layout with a working module and controller

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
 //Module
-var courseApp = angular.module('courseApp', []);
+var courseApp = angular.module('courseApp', ['ngRoute']);
+
+
+// Configure the routes
+courseApp.config(function($routeProvider){
+
+	$routeProvider
+
+		//route for Home page
+		.when('/', {
+			templateUrl : 'home.html',
+			controller : 'mainController'
+		})
+
+		//route for courses
+		.when('/courses', {
+			templateUrl : 'courses.html',
+			controller : 'courseController'
+		})
+
+		.when('/about', {
+			templateUrl : 'about.html',
+			controller : 'aboutController'
+		})
+
+		.when('/contact', {
+			templateUrl : 'contact.html',
+			controller : 'contactController'
+		});
+});
 
 //Controller
 courseApp.controller('mainController', function($scope){
-
 	//Test
 	$scope.message = 'Hello world';
 });
+
+courseApp.controller('courseController', function($scope){
+	$scope.message = 'Im the courses';
+});
+
+courseApp.controller('aboutController', function($scope){
+	$scope.message = 'Im all about the bass';
+});
+
+courseApp.controller('contactController', function($scope){
+	$scope.message = 'Contact mee pleeaase';
+});
+
+
+
+
+
+
+
