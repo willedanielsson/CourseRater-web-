4ecae79d38a80f322960850c812ac48030295e49
William Danielsson <William@williams-mbp.lan>
2014-12-10 19:11:52 +0100

    Began my own experiment

---
+++ script.js
@@ -0,0 +1,9 @@
+//Module
+var courseApp = angular.module('courseApp', []);
+
+//Controller
+courseApp.controller('mainController', function($scope){
+
+	//Test
+	$scope.message = 'Hello world';
+});
