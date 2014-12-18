e4ab99c306a0f0b3f903668178b3972cf3456486
William Danielsson <William@williams-mbp.lan>
2014-12-18 19:19:56 +0100

    Having a switch on the container of ratings which will be switched to a comment-container when a course-part has been pressed

--- script/course_angular.js
+++ script/course_angular.js
@@ -1,6 +1,5 @@
  courseApp.controller('courseController', function($scope, $http){
 
-
 	$http.get("http://www.ashiya.se/app/getCountries.php").success(function(response){
 		$scope.countries = response;
 	});
@@ -9,6 +8,8 @@
   		$http.get("http://www.ashiya.se/app/getUniversitiesForCountry.php?country="+country).success(function(response){
   			$scope.universities = response;
   		});
+
+  		// When a country has been chosen, show that the university-list is unlocked
   		$scope.styleSelectUniversity = {
   			'pointer-events':"visible",
   			'border': "2px solid green"
@@ -19,7 +20,7 @@
 		$http.get("http://www.ashiya.se/app/getCoursesForUniversity.php?chosenUniversity="+university).success(function(response){
 			$scope.courses = response;
 		})
-
+		// When a university has been chosen, show that the course-list is unlocked
 		$scope.styleSelectCourse = {
   			'pointer-events':"visible",
   			'border': "2px solid green"
@@ -27,7 +28,8 @@
 	}
 
 	$scope.clickedCourse = function(){
-		console.log("Chose a course");
+
+		// When a course has been chosen, display that the 'View Course'-button can be clicked
 		$scope.styleViewCourseButton = {
 			'border': "2px solid green"
 		}
@@ -35,18 +37,20 @@
 		button.removeAttr('disabled');
 	}
 
-	$scope.getCourseInformation = function(course){
-		$http.get("http://www.ashiya.se/app/getCourseInformation.php?chosenCourse="+course).success(function(response){
-			//var caseR = response.Ratings[0]['AVG(cI.usefulness)'];
-			//console.log(caseR);
+	$scope.getCourseInformation = function(course, ratings_or_comments){
 
+		//When pressed 'View Course', we display the ratings by default
+		$scope.ratings_or_comments = ratings_or_comments;
+
+		$scope.course = course;
+		$http.get("http://www.ashiya.se/app/getCourseInformation.php?chosenCourse="+course).success(function(response){
+
+			//Give the ratings their needed data
 			$scope.courseInformation = response;
-			$scope.course = course;
 
 			console.log(response.Ratings[0]);
 		});
 	}
 
 
-	
 });
