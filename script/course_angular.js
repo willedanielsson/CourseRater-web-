85ca8b17758e7603fe820c85b0e92daedb5b1048
William Danielsson <William@williams-mbp.lan>
2014-12-19 09:54:52 +0100

    Cleaned up

--- script/course_angular.js
+++ script/course_angular.js
@@ -52,5 +52,15 @@
 		});
 	}
 
+	
 
 });
+
+courseApp.controller('course_information_controller', function($scope){
+
+	$scope.getComments = function(){
+		console.log("Click");
+	}
+
+});
+
---
+++ script/course_information_angular.js
@@ -0,0 +1,8 @@
+
+courseApp.controller('course_information_controller', function($scope){
+
+	$scope.getComments = function(){
+		console.log("Click");
+	}
+
+});
